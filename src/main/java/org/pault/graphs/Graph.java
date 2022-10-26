baaf2ebc910fb347b5f26c842aecad9a02c1964e
Paul Thomas <paulthomas2107@gmail.com>
2022-10-26 18:59:03 +0100

    Initial

---
+++ src/main/java/org/pault/graphs/Graph.java
@@ -0,0 +1,11 @@
+package org.pault.graphs;
+
+import lombok.extern.java.Log;
+
+@Log
+public class Graph {
+    public static void main(String[] args) {
+
+        log.info("Hello Paul !.....");
+    }
+}
